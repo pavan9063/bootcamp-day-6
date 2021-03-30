5964492f99a848c5e4b3a52a754ff40353f5ed41
pavan <pavan.satyala@fisclouds.com>
2021-03-30 14:12:34 +0530

    Added test suites to katalon

---
+++ Test Suites/bootcamp day 6/bootcamp day 6 Ts.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>bootcamp day 6 Ts</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6c85465a-af53-439f-bf67-811f11ba6414</testSuiteGuid>
+</TestSuiteEntity>
